bba1f0474883934fac4289e5b3a77ccf8feee073
Adrian F Gioia <102843983+eighdreeuhn@users.noreply.github.com>
2022-04-23 10:00:23 -0700

    Add files via upload

---
+++ fibonacci/fibonacci.js
@@ -0,0 +1,12 @@
+// function fibonacci() {
+//     const box = document.getElementsByClassName('.square');
+//     let d = 1
+//     ${box}.css(backgroundcolor, )
+//     document.body.appendChild('square')
+//     ${}
+//     for (let i = 0; i < 12; i++) {
+
+//     }
+// }
+
+$("#wrapper").css('background-color', 'red');
